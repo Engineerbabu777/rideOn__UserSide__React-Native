50b72960599b5621b399ca67b68ad40ab1ae2b92
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-12-14 20:22:02 +0500

    Add:/ onboarding screen and login screens

---
+++ app/(routes)/onboarding/index.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import OnBoardingScreen from '@/screens/onboarding/onboarding.screen'
+
+export default function index() {
+  return (
+   <OnBoardingScreen />
+  )
+}
+
+const styles = StyleSheet.create({})
