1feef1b458bd5a97c35c9d2fd4a20d84120b67c7
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-12-16 14:52:25 +0500

    add:/ created email verification route

---
+++ app/(routes)/email-verification/index.tsx
@@ -0,0 +1,6 @@
+// import EmailVerificationScreen from "@/screens/email-verification/email.verification.screen";
+
+export default function index() {
+  // return <EmailVerificationScreen />;
+  return<></>
+}
